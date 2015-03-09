bc6ddf5b66a42273f5f2d359f9e4a42a941d44c9
FranekF <marcin.frankowski@workmail.com>
2015-03-10 00:08:09 +0100

    first commit

--- src/mfrankowski/test/Test.java
+++ src/mfrankowski/test/Test.java
@@ -2,8 +2,8 @@ package mfrankowski.test;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		System.out.println("first commit");
 	}
 
